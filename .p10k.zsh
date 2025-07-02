4b8208ce4d450c76e078f45087cef033ec8c241f
Vincent THOME <69122293+S7VEN-7@users.noreply.github.com>
2025-07-02 13:49:18 +0200

    Update .p10k.zsh

--- .p10k.zsh
+++ .p10k.zsh
@@ -107,10 +107,10 @@
     per_directory_history   # Oh My Zsh per-directory-history local/global indicator
     # cpu_arch              # CPU architecture
     time                    # current time
+    public_ip             # public IP address
     # =========================[ Line #2 ]=========================
     newline
     ip                    # ip address and bandwidth usage for a specified network interface
-    # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
     battery               # internal battery
     # wifi                  # wifi speed
